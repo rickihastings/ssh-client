94a5ab7cfe59b045c295839e550c71e9f5dd8e7d
Ricki <ricki@ircanywhere.com>
2012-09-22 23:34:36 +0100

    Latest update, working window controls
    Fully working window controls, apart from the icon changing on
    maximising.

--- ssh-client/WindowControls.xaml.cs
+++ ssh-client/WindowControls.xaml.cs
@@ -12,15 +12,69 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace ssh_client
-{
+{	
 	/// <summary>
 	/// Interaction logic for WindowControls.xaml
 	/// </summary>
 	public partial class WindowControls : UserControl
 	{
+		public bool WindowMaximized = false;
+		public double WindowWidth;
+		public double WindowHeight;
+		public double WindowLeft;
+		public double WindowTop;
+		
 		public WindowControls()
 		{
 			this.InitializeComponent();
+        }
+		
+		protected void MinimiseButtonClick(object sender, RoutedEventArgs e)
+		{
+			// Get the window, and change it to Minimized
+			Window window = Window.GetWindow(this);
+			window.WindowState = WindowState.Minimized;
+		}
+		
+		protected void MaximizeButtonClick(object sender, RoutedEventArgs e)
+		{
+			// Get the window, and determine whether we need to maximise it or not.
+			Window window = Window.GetWindow(this);
+			if (!this.WindowMaximized)
+			{
+				// save current dimensions
+				this.WindowMaximized = true;
+				this.WindowHeight = window.Height;
+				this.WindowWidth = window.Width;
+				this.WindowLeft = window.Left;
+				this.WindowTop = window.Top;
+				
+				// new dimensions (maximized)
+				window.Height = SystemParameters.WorkArea.Height + 10;
+				window.Width = SystemParameters.WorkArea.Width + 10;
+				window.Left = -5;
+				window.Top = -5;
+				window.ResizeMode = ResizeMode.NoResize;
+				
+			}
+            else
+			{
+                // alter windowMaximized variable
+				this.WindowMaximized = false;
+				
+				// new dimensions (minimized)
+				window.Height = this.WindowHeight;
+				window.Width = this.WindowWidth;
+				window.Left = this.WindowLeft;
+				window.Top = this.WindowTop;
+				window.ResizeMode = ResizeMode.CanResizeWithGrip;
+			}
+		}
+		
+		protected void CloseButtonClick(object sender, RoutedEventArgs e)
+		{
+			// shutdown the application properly
+			ssh_client.App.Current.Shutdown();
 		}
 	}
 }
