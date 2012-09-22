a1862d7097e4a3fdeca57658909c44f5ab8d6ab7
Ricki <ricki@ircanywhere.com>
2012-09-22 12:17:51 +0100

    Initial commit, basic window with no controls
    working resize grip and basic button style setup.

---
+++ ssh-client/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ssh_client
+{
+	/// <summary>
+	/// Interaction logic for Window1.xaml
+	/// </summary>
+
+	public partial class MainWindow : System.Windows.Window
+	{
+        public MainWindow()
+		{
+			//this.InitializeComponent();
+		}
+    }
+}
